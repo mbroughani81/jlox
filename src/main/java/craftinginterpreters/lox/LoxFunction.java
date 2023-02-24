b2b39b1a655c92dbd2a5703a22dbabec08dd5d74
Mohammad Broughani <gitmbroughani81@gmail.com>
2023-02-25 03:12:28 +0330

    add closure

--- src/main/java/craftinginterpreters/lox/LoxFunction.java
+++ src/main/java/craftinginterpreters/lox/LoxFunction.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 class LoxFunction implements LoxCallable {
     private final Stmt.Function declaration;
+    private final Environment closure;
 
-    LoxFunction(Stmt.Function declaration) {
+    LoxFunction(Stmt.Function declaration, Environment closure) {
+        this.closure = closure;
         this.declaration = declaration;
     }
 
@@ -14,11 +16,11 @@ class LoxFunction implements LoxCallable {
     }
     
     public Object call(Interpreter interpreter, List<Object> arguments) {
-        Environment environment = new Environment(interpreter.globals);
+        Environment environment = new Environment(closure);
         for (int i = 0; i < declaration.params.size(); i++) {
             environment.define(declaration.params.get(i).lexeme, arguments.get(i));
         }
-        
+
         try {
             interpreter.executeBlock(declaration.body, environment); 
         } catch (Return returnValue) {
